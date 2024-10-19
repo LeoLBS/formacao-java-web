681b3648cb395fc9541f11f456a1d497fed8bf0c
LeoLBS <leonardoperin70@gmail.com>
2024-10-19 19:48:27 -0300

    feat: add PrimeiraClasseJavaPoo ( Criando nossa classe com o metodo main ) and Aluno ( Criando nossa classe Aluno onde temos seus atributos )

---
+++ Java_POO/primeiro-programa-java-poo/src/cursojava/classes/Aluno.java
@@ -0,0 +1,18 @@
+package cursojava.classes;
+
+/*Aqui temos nossa classe Aluno, sendo ela o nosso molde para os objetos*/
+public class Aluno {
+	
+	/*Aqui temos os nossos atributos da nossa classe Aluno*/
+	String nomeAluno;
+	int idadeAluno;
+	String dataNascimentoAluno;
+	String registroGeralAluno;
+	String numeroCpfAluno;
+	String nomeMaeAluno;
+	String nomePaiAluno;
+	String dataMatriculaAluno;
+	String nomeEscolaAluno;
+	String serieMatriculadoAluno;
+	
+}
---
+++ Java_POO/primeiro-programa-java-poo/src/cursojava/exercicios/PrimeiraClasseJavaPoo.java
@@ -0,0 +1,10 @@
+package cursojava.exercicios;
+
+/*Aqui temos a nossa classe principal, dentro dela tem a main para executarmos no app*/
+public class PrimeiraClasseJavaPoo {
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+	}
+}
