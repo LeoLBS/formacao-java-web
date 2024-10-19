faca7d6c4d0061826469d52e49325741e10dc191
LeoLBS <leonardoperin70@gmail.com>
2024-10-19 20:45:55 -0300

    update: docs PrimeiraClasseJavaPoo ( Criando e definindo os objetos da classe Aluno )

--- Java_POO/primeiro-programa-java-poo/src/cursojava/exercicios/PrimeiraClasseJavaPoo.java
+++ Java_POO/primeiro-programa-java-poo/src/cursojava/exercicios/PrimeiraClasseJavaPoo.java
@@ -1,10 +1,18 @@
 package cursojava.exercicios;
 
+import cursojava.classes.Aluno;
+
 /*Aqui temos a nossa classe principal, dentro dela tem a main para executarmos no app*/
 public class PrimeiraClasseJavaPoo {
 	
 	/*Main é um metodo auto executavel em Java*/
 	public static void main(String[] args) {
+		
+		Aluno aluno1; // Objeto não definido na memoria
+		aluno1 = new Aluno(); // Definindo o objeto na memoria
+		
+		Aluno aluno2 = new Aluno(); // Criando e definindo o objeto na memoria
+		
 		
 	}
 }
