0d9f8d2f747e97a8e9cb746fad7826423671398b
LeoLBS <leonardoperin70@gmail.com>
2024-10-30 12:54:10 -0300

    update: docs PrimeiraClasseJavaPoo ( Trabalhando com o JOptionPane para receber os dados do aluno )

--- Java_POO/primeiro-programa-java-poo/src/cursojava/exercicios/PrimeiraClasseJavaPoo.java
+++ Java_POO/primeiro-programa-java-poo/src/cursojava/exercicios/PrimeiraClasseJavaPoo.java
@@ -1,5 +1,7 @@
 package cursojava.exercicios;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 /*Aqui temos a nossa classe principal, dentro dela tem a main para executarmos no app*/
@@ -8,23 +10,39 @@ public class PrimeiraClasseJavaPoo {
 	/*Main é um metodo auto executavel em Java*/
 	public static void main(String[] args) {
 		
+		String nomeAluno = JOptionPane.showInputDialog("Qual o nome do aluno: ");
+		String idadeAluno = JOptionPane.showInputDialog("Qual a idade do aluno: ");
+		String dataNascimentoAluno = JOptionPane.showInputDialog("Qual a data de nascimento do aluno: ");
+		String registroGeralAluno = JOptionPane.showInputDialog("Qual o RG do aluno: ");
+		String numeroCpfAluno = JOptionPane.showInputDialog("Qual o CPF do aluno: ");
+		String nomeMaeAluno = JOptionPane.showInputDialog("Qual o nome da mae do aluno: ");
+		String nomePaiAluno = JOptionPane.showInputDialog("Qual o nome do pai do aluno: ");
+		String dataMatriculaAluno = JOptionPane.showInputDialog("Qual a data da matricula do aluno: ");
+		String serieMatriculadoAluno = JOptionPane.showInputDialog("Qual a serie do aluno: ");
+		String nomeEscolaAluno = JOptionPane.showInputDialog("Qual o nome da escola do aluno: ");
+		
+		String nota1 = JOptionPane.showInputDialog("Qual a nota 1 do aluno: ");
+		String nota2 = JOptionPane.showInputDialog("Qual a nota 2 do aluno: ");
+		String nota3 = JOptionPane.showInputDialog("Qual a nota 3 do aluno: ");
+		String nota4 = JOptionPane.showInputDialog("Qual a nota 4 do aluno: ");
+		
 		Aluno aluno1 = new Aluno(); // Criando e definindo o objeto na memoria
 		
-		aluno1.setNomeAluno("Pedro"); // Utilizando o metodo set para definir o valor da variavel
-		aluno1.setIdadeAluno(18);
-		aluno1.setDataNascimentoAluno("20/09/2006");
-		aluno1.setRegistroGeralAluno("25364.435333.4657");
-		aluno1.setNumeroCpfAluno("723.723.723-99");
-		aluno1.setNomeMaeAluno("Maria");
-		aluno1.setNomePaiAluno("Carlos");
-		aluno1.setDataMatriculaAluno("10/04/2018");
-		aluno1.setSerieMatriculadoAluno("3");
-		aluno1.setNomeEscolaAluno("Escola de treinamento DEV");
+		aluno1.setNomeAluno(nomeAluno); // Utilizando o metodo set para definir o valor da variavel
+		aluno1.setIdadeAluno(Integer.valueOf(idadeAluno));
+		aluno1.setDataNascimentoAluno(dataNascimentoAluno);
+		aluno1.setRegistroGeralAluno(registroGeralAluno);
+		aluno1.setNumeroCpfAluno(numeroCpfAluno);
+		aluno1.setNomeMaeAluno(nomeMaeAluno);
+		aluno1.setNomePaiAluno(nomePaiAluno);
+		aluno1.setDataMatriculaAluno(dataMatriculaAluno);
+		aluno1.setSerieMatriculadoAluno(serieMatriculadoAluno);
+		aluno1.setNomeEscolaAluno(nomeEscolaAluno);
 		
-		aluno1.setNota1(90);
-		aluno1.setNota2(40.5);
-		aluno1.setNota3(70);
-		aluno1.setNota4(85);
+		aluno1.setNota1(Double.valueOf(nota1));
+		aluno1.setNota2(Double.valueOf(nota2));
+		aluno1.setNota3(Double.valueOf(nota3));
+		aluno1.setNota4(Double.valueOf(nota4));
 		
 		System.out.println("Nome do aluno é: " + aluno1.getNomeAluno()); // Buscando o valor definido na variavel com o metodo get
 		System.out.println("Idade do aluno é: " + aluno1.getIdadeAluno());
@@ -40,12 +58,5 @@ public class PrimeiraClasseJavaPoo {
 		System.out.println("Média do Aluno: " + aluno1.getMediaNota());
 		System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 		
-		Aluno aluno2 = new Aluno();
-		
-		Aluno aluno3 = new Aluno();
-		
-		Aluno aluno4 = new Aluno("Leonardo"); // Criando um objeto com parametro ja definido do construtor
-		
-		Aluno aluno5 = new Aluno("João", 16); 
 	}
 }
