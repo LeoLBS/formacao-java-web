3ad24958bc1416ff7c669c5074fd7630df4fcda2
LeoLBS <leonardoperin70@gmail.com>
2025-09-08 21:57:19 -0300

    Adicionando a pratica 03: Primitivos em ação

---
+++ Java_Pratica/pratica03_primitivos_acao/src/Main.java
@@ -0,0 +1,14 @@
+//Você está desenvolvendo um aplicativo que calcula a média de notas dos alunos.
+//Para isso, você precisa armazenar as notas em variáveis do tipo double.
+//Você decide criar três variáveis chamadas nota1, nota2 e nota3,
+//e atribuir a elas os valores 7.5, 8.0 e 9.0, respectivamente
+//e em seguida, calcular a média e exibi-la na tela.
+
+
+public class Main {
+    public static void main(String[] args) {
+        double nota1 = 7.5, nota2 = 8.0, nota3 = 9.0;
+
+        System.out.println("Média das notas: " + ((nota1 + nota2 + nota3)/3));
+    }
+}
