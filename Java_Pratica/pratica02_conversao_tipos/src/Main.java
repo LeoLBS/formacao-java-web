37e4d96567278eee1319c0c1489d391e41091392
LeoLBS <leonardoperin70@gmail.com>
2025-09-08 21:44:33 -0300

    Adicionando prática 02: Conversão de tipos

---
+++ Java_Pratica/pratica02_conversao_tipos/src/Main.java
@@ -0,0 +1,13 @@
+//Para atender às exigências do sistema, crie um programa que converta a
+//quantidade de double para int, garantindo que os valores sejam adequados ao formato aceito.
+
+public class Main {
+    public static void main(String[] args) {
+        double valorDouble = 152.95;
+
+        int valorInteiro = (int) valorDouble;
+
+        System.out.println("Valor inteiro do produto: " + valorInteiro);
+
+    }
+}
