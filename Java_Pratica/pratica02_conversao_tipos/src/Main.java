c18a5c105c439a4252d6ecb48e27f576fe45785d
LeoLBS <leonardoperin70@gmail.com>
2025-09-08 21:51:19 -0300

    Adicionando os métodos de Math

--- Java_Pratica/pratica02_conversao_tipos/src/Main.java
+++ Java_Pratica/pratica02_conversao_tipos/src/Main.java
@@ -3,11 +3,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        double valorDouble = 152.95;
+        double valorDouble = 152.9;
 
         int valorInteiro = (int) valorDouble;
 
         System.out.println("Valor inteiro do produto: " + valorInteiro);
 
+        valorInteiro = (int) Math.ceil(valorDouble);
+
+        System.out.println("Arredondando para cima: " + valorInteiro);
+
+        valorInteiro = (int) Math.floor(valorDouble);
+
+        System.out.println("Arredondando para baixo: " + valorInteiro);
     }
 }
